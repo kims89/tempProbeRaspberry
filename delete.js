3ca66ab09527bd6c95a680e231b70d79fee3ff3b
Kim Tore Kristensen <kimelim89@gmail.com>
2017-04-19 22:32:53 +0200

    v2

--- delete.js
+++ delete.js
@@ -1,9 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  db.remove({}, function(err) {
-    console.log('collection removed')
-  });
+mongoose.connect('mongodb://localhost/temp1');
+mongoose.connection.collections['temperature'].drop(function(err) {
+  console.log('collection dropped');
 });
