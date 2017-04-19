03eda3aca214cec01f6d0968e10d171dc4bc66c0
Kim Tore Kristensen <kimelim89@gmail.com>
2017-04-19 23:22:45 +0200

    ddsdsd

--- pyTemp.js
+++ pyTemp.js
@@ -1,45 +1,46 @@
 var sensor = require('node-dht-sensor');
 var mongoose = require('mongoose');
 var cron = require('node-cron');
+cron.schedule('0 0 * * * *', function() {
+  mongoose.Promise = global.Promise;
+  mongoose.connect('mongodb://localhost/temp');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/temp');
+  var db = mongoose.connection;
 
-var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-
-  var tempSchema = mongoose.Schema({
-    date: Date,
-    humidity: Number,
-    temp: Number
+    var tempSchema = mongoose.Schema({
+      date: Date,
+      humidity: Number,
+      temp: Number
 
 
-  });
-  var Temperature = mongoose.model('temperature', tempSchema);
+    });
+    var Temperature = mongoose.model('temperature', tempSchema);
 
-  sensor.read(11, 4, function(err, temperature, humidity) {
-    if (!err) {
-      var tempNow = new Temperature({
-        date: Date(),
-        humidity: humidity.toFixed(1),
-        temp: temperature.toFixed(1)
-      });
+    sensor.read(11, 4, function(err, temperature, humidity) {
+      if (!err) {
+        var tempNow = new Temperature({
+          date: Date(),
+          humidity: humidity.toFixed(1),
+          temp: temperature.toFixed(1)
+        });
 
-      tempNow.save(function(err, data) {
-        if (err) return console.error(err);
-        mongoose.disconnect();
-      });
+        tempNow.save(function(err, data) {
+          if (err) return console.error(err);
+          mongoose.disconnect();
+        });
 
-      console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-        'humidity: ' + humidity.toFixed(1) + '%'
-      );
-    }
-  });
+        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+          'humidity: ' + humidity.toFixed(1) + '%'
+        );
+      }
+    });
 
-  Temperature.find(function(err, data) {
-    if (err) return console.error(err);
-    console.log(data);
+    Temperature.find(function(err, data) {
+      if (err) return console.error(err);
+      console.log(data);
+    });
   });
 });
