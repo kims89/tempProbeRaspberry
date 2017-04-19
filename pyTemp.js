d34cc13f1ed759c8dd882500181b6d644ba5fe88
Kim Tore Kristensen <kimelim89@gmail.com>
2017-04-19 23:18:14 +0200

    xxcdf

--- pyTemp.js
+++ pyTemp.js
@@ -1,7 +1,7 @@
 var sensor = require('node-dht-sensor');
 var mongoose = require('mongoose');
-var schedule = require('node-schedule');
-schedule.schedule('0 0 * * * *', function() {
+var cron = require('node-cron');
+cron.schedule('0 0 * * * *', function() {
   mongoose.Promise = global.Promise;
   mongoose.connect('mongodb://localhost/temp');
 
