b3d73d3077d47bb7417441e418937268a4b62fe1
Kim <kimelim89@gmail.com>
2017-04-19 22:47:17 +0200

    v6

--- delete.js
+++ delete.js
@@ -1,5 +1,3 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/temp1');
-mongoose.connection.db.dropDatabase(function(err) {
-  console.log('db dropped');
-});
+mongoose.connection.db.dropDatabase(function(err, result) {});
