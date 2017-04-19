b8f161a7e31768428ec256a070188f3b05e5bff8
Kim Tore Kristensen <kimelim89@gmail.com>
2017-04-19 22:21:50 +0200

    temp updt

--- db.js
+++ db.js
@@ -5,16 +5,18 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   var kittySchema = mongoose.Schema({
     name: String
-});
-var Kitten = mongoose.model('Kitten', kittySchema);
-var silence = new Kitten({ name: 'Silence2' });
-silence.save(function (err, fluffy) {
-  if (err) return console.error(err);
+  });
+  var Kitten = mongoose.model('Kitten', kittySchema);
+  var silence = new Kitten({
+    name: 'Silence2'
+  });
+  silence.save(function(err, fluffy) {
+    if (err) return console.error(err);
 
-});
+  });
 
-Kitten.find(function (err, kittens) {
-  if (err) return console.error(err);
-  console.log(kittens);
-})
+  Kitten.find(function(err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+  })
 });
--- pyTemp.js
+++ pyTemp.js
@@ -1,12 +1,42 @@
-
 var sensor = require('node-dht-sensor');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/temp1');
 
-sensor.read(11, 4, function(err, temperature, humidity) {
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+
+  var tempSchema = mongoose.Schema({
+    date: Date,
+    humidity: Number,
+    temp: Number
+
+
+  });
+  var Temperature = mongoose.model('temperature', tempSchema);
+
+  sensor.read(11, 4, function(err, temperature, humidity) {
     if (!err) {
-        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-            'humidity: ' + humidity.toFixed(1) + '%'
-        );
+      var tempNow = new Temperature({
+        date: Date(),
+        humidity: humidity.toFixed(1),
+        temp: temperature.toFixed(1)
+      });
+
+      tempNow.save(function(err, data) {
+        if (err) return console.error(err);
+      });
+
+      console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+        'humidity: ' + humidity.toFixed(1) + '%'
+      );
     }
+  });
+
+
+  Temperature.find(function(err, data) {
+    if (err) return console.error(err);
+    console.log(data);
+  })
 });
-
-
