f89960a6b64db8776d1190497381e71f6e83a1d0
Kim Tore Kristensen <kimelim89@gmail.com>
2017-04-23 23:55:32 +0200

    failsearch

--- pyTempCheck.js
+++ pyTempCheck.js
@@ -1,11 +1,13 @@
 var mongoose = require('mongoose');
-var TempDB = require('./temp.js');
+var TempDBB = require('./temp.js');
 
 var uri = 'mongodb://localhost/TempLogDB';
 var connection = mongoose.createConnection(uri);
 
+var temp = TempDBB();
 
-TempDB.find(function(err, results) {
+//mongoose get all docs. I think here answers your question directly
+temp.find(function(err, results) {
   if (err) return console.error(err);
 
   //invoke callback with your mongoose returned result
