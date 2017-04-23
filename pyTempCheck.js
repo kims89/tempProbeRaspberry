513126181c1863a940034dd69515fee9ee8f011b
Kim Tore Kristensen <kimelim89@gmail.com>
2017-04-23 23:52:29 +0200

    remove trash

--- pyTempCheck.js
+++ pyTempCheck.js
@@ -1,23 +1,11 @@
 var mongoose = require('mongoose');
+var TempDB = require('./temp.js');
 
-//connection to Mongodb instance running on=======
-//local machine or anywhere=========================
 var uri = 'mongodb://localhost/TempLogDB';
 var connection = mongoose.createConnection(uri);
 
 
-//Define Schema==================================
-var Schema = mongoose.Schema;
-var BlogPostSchema = new Schema({
-  date: Date,
-  humidity: Number,
-  temp: Number
-});
-
-var BlogPostModel = connection.model('temperature', BlogPostSchema);
-
-//mongoose get all docs. I think here answers your question directly
-BlogPostModel.find(function(err, results) {
+TempDB.find(function(err, results) {
   if (err) return console.error(err);
 
   //invoke callback with your mongoose returned result
