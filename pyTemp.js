f8224aa230d9d08eb55cc751dd2993169e58ae12
Kim Tore Kristensen <kimelim89@gmail.com>
2017-04-23 21:58:27 +0200

    blooo

--- pyTemp.js
+++ pyTemp.js
@@ -8,21 +8,22 @@ mongoose.connect('mongodb://localhost/TemperaturLogVen');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
+  cron.schedule('0 * * * * *', function() {
+    sensor.read(11, 4, function(err, temperature, humidity) {
+      console.log(Date() + " - Fullfort");
+      if (!err) {
+        var tempNow = new TempDB({
+          date: Date(),
+          humidity: humidity.toFixed(1),
+          temp: temperature.toFixed(1)
+        });
 
-  sensor.read(11, 4, function(err, temperature, humidity) {
-    console.log(Date() + " - Fullfort");
-    if (!err) {
-      var tempNow = new TempDB({
-        date: Date(),
-        humidity: humidity.toFixed(1),
-        temp: temperature.toFixed(1)
-      });
-
-      tempNow.save(function(err, data) {
-        if (err) return console.error(err);
-        mongoose.disconnect();
-      });
-    }
+        tempNow.save(function(err, data) {
+          if (err) return console.error(err);
+          mongoose.disconnect();
+        });
+      }
+    });
   });
 
 });
