77416a04e69e12d83a67f62e548925a7ccb2793b
Kim Tore Kristensen <kimelim89@gmail.com>
2017-04-23 22:19:16 +0200

    boom

--- pyTemp.js
+++ pyTemp.js
@@ -5,15 +5,16 @@ var cron = require('node-cron');
 cron.schedule('0 * * * * *', function() {
   mongoose.Promise = global.Promise;
   mongoose.connect('mongodb://localhost/TemperaturLogDB');
+  dato = Date();
 
   var db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
     sensor.read(11, 4, function(err, temperature, humidity) {
-      console.log(Date() + " - Fullfort");
+      console.log(dato + " - Fullfort");
       if (!err) {
         var tempNow = new TempDB({
-          date: Date(),
+          date: dato,
           humidity: humidity.toFixed(1),
           temp: temperature.toFixed(1)
         });
