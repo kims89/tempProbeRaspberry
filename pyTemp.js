10b13733f4d68f663a1c442a1cab9693a300c3fd
Kim <kimelim89@gmail.com>
2017-04-26 17:42:33 +0200

    xxx

--- pyTemp.js
+++ pyTemp.js
@@ -2,31 +2,30 @@ var sensor = require('node-dht-sensor');
 var mongoose = require('mongoose');
 var TempDB = require('./temp.js');
 var cron = require('node-cron');
-cron.schedule('40 * * * *', function() {
+cron.schedule('44 * * * *', function() {
   var dato = new Date();
   dato.setHours(dato.getHours() + 2);
   mongoose.Promise = global.Promise;
   db = mongoose.createConnection('mongodb://localhost/templogh');
   db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function()
+  db.once('open', function() {
 
-  var tempSchema = mongoose.Schema({
-    date: Date,
-    humidity: Number,
-    temp: Number
+        var tempSchema = mongoose.Schema({
+          date: Date,
+          humidity: Number,
+          temp: Number
 
-
-  });
-  var Temperature = mongoose.model('temperature', tempSchema);
+        });
+        var Temperature = mongoose.model('temperature', tempSchema);
 
     sensor.read(11, 4, function(err, temperature, humidity) {
       console.log(dato + " - Fullfort");
       if (!err) {
         var tempNow = new Temperature({
-          date: dato,
-          humidity: humidity.toFixed(1),
-          temp: temperature.toFixed(1)
-        });
+             date: Date(),
+             humidity: humidity.toFixed(1),
+             temp: temperature.toFixed(1)
+           });
 
         tempNow.save(function(err, data) {
           console.log("Lagret");
