d661fc8171df2e33e570cb3444b545d6b61ea639
Kim <kimelim89@gmail.com>
2017-04-26 17:48:46 +0200

    bøm

--- pyTemp.js
+++ pyTemp.js
@@ -2,37 +2,27 @@ var sensor = require('node-dht-sensor');
 var mongoose = require('mongoose');
 var TempDB = require('./temp.js');
 var cron = require('node-cron');
-cron.schedule('44 * * * *', function() {
+cron.schedule('50 * * * *', function() {
   var dato = new Date();
   dato.setHours(dato.getHours() + 2);
   mongoose.Promise = global.Promise;
   db = mongoose.createConnection('mongodb://localhost/templogh');
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
-
-        var tempSchema = mongoose.Schema({
-          date: Date,
-          humidity: Number,
-          temp: Number
-
-        });
-        var Temperature = mongoose.model('temperature', tempSchema);
-
     sensor.read(11, 4, function(err, temperature, humidity) {
-      console.log(dato + " - Fullfort");
+      console.log(Date() + " - Fullfort");
       if (!err) {
-        var tempNow = new Temperature({
-             date: Date(),
-             humidity: humidity.toFixed(1),
-             temp: temperature.toFixed(1)
-           });
-
-        tempNow.save(function(err, data) {
-          console.log("Lagret");
-          if (err) return console.error(err);
-          mongoose.disconnect();
+        var tempNow = new TempDB({
+          date: dato,
+          humidity: humidity.toFixed(1),
+          temp: temperature.toFixed(1)
         });
       }
+    });
+    tempNow.save(function(err, data) {
+      console.log("Lagret");
+      if (err) return console.error(err);
+      mongoose.disconnect();
     });
   });
 
