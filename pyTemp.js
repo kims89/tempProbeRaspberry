ef8318706bc94ea78f27e025da83c4e0bfb70919
Kim <kimelim89@gmail.com>
2017-04-26 17:37:57 +0200

    dd

--- pyTemp.js
+++ pyTemp.js
@@ -2,17 +2,27 @@ var sensor = require('node-dht-sensor');
 var mongoose = require('mongoose');
 var TempDB = require('./temp.js');
 var cron = require('node-cron');
-cron.schedule('16 * * * *', function() {
+cron.schedule('40 * * * *', function() {
   var dato = new Date();
   dato.setHours(dato.getHours() + 2);
   mongoose.Promise = global.Promise;
   db = mongoose.createConnection('mongodb://localhost/templogh');
   db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function() {
+  db.once('open', function()
+
+  var tempSchema = mongoose.Schema({
+    date: Date,
+    humidity: Number,
+    temp: Number
+
+
+  });
+  var Temperature = mongoose.model('temperature', tempSchema);
+
     sensor.read(11, 4, function(err, temperature, humidity) {
-      console.log(Date() + " - Fullfort");
+      console.log(dato + " - Fullfort");
       if (!err) {
-        var tempNow = new TempDB({
+        var tempNow = new Temperature({
           date: dato,
           humidity: humidity.toFixed(1),
           temp: temperature.toFixed(1)
