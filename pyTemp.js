66b3424b782f9f6886dd81d1c886673af3d44304
Kim Tore Kristensen <kimelim89@gmail.com>
2017-05-12 22:03:45 +0200

    remove pm2

--- pyTemp.js
+++ pyTemp.js
@@ -4,30 +4,27 @@ var Schema = mongoose.Schema;
 var cron = require('node-cron');
 var TempDB = require('./temp.js');
 
-cron.schedule('02 * * * *', function() {
-  var dato = new Date();
-  dato.setHours(dato.getHours() + 2);
-  mongoose.Promise = global.Promise;
-  db = mongoose.createConnection('mongodb://localhost/templogh');
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function() {
+var dato = new Date();
+dato.setHours(dato.getHours() + 2);
+mongoose.Promise = global.Promise;
+db = mongoose.createConnection('mongodb://localhost/templogh');
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
 
-  });
+});
 
-  sensor.read(11, 4, function(err, temperature, humidity) {
-    console.log(Date() + " - Fullfort");
-    if (!err) {
-      var tempNow = new TempDB({
-        date: dato,
-        humidity: humidity.toFixed(1),
-        temp: temperature.toFixed(1)
-      });
-      tempNow.save(function(err) {
-        console.log("Lagret");
-        if (err) return handleError(err);
-        mongoose.disconnect();
-      });
-    }
-  });
-
+sensor.read(11, 4, function(err, temperature, humidity) {
+  console.log(Date() + " - Fullfort");
+  if (!err) {
+    var tempNow = new TempDB({
+      date: dato,
+      humidity: humidity.toFixed(1),
+      temp: temperature.toFixed(1)
+    });
+    tempNow.save(function(err) {
+      console.log("Lagret");
+      if (err) return handleError(err);
+      mongoose.disconnect();
+    });
+  }
 });
