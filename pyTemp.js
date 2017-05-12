382fe1f27d5f0faebb47d4cf79e9f1acc16e0bd1
Kim Tore Kristensen <kimelim89@gmail.com>
2017-05-12 22:05:21 +0200

    fix db

--- pyTemp.js
+++ pyTemp.js
@@ -9,9 +9,7 @@ dato.setHours(dato.getHours() + 2);
 mongoose.Promise = global.Promise;
 db = mongoose.createConnection('mongodb://localhost/templogh');
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
 
-});
 
 sensor.read(11, 4, function(err, temperature, humidity) {
   console.log(Date() + " - Fullfort");
@@ -21,10 +19,12 @@ sensor.read(11, 4, function(err, temperature, humidity) {
       humidity: humidity.toFixed(1),
       temp: temperature.toFixed(1)
     });
-    tempNow.save(function(err) {
-      console.log("Lagret");
-      if (err) return handleError(err);
-      mongoose.disconnect();
+    db.once('open', function() {
+      tempNow.save(function(err) {
+        console.log("Lagret");
+        if (err) return handleError(err);
+        mongoose.disconnect();
+      });
     });
   }
 });
