85d4ce3c7bf1265a580f01c5d9873e5a8d8a3a7c
Kim Tore Kristensen <kimelim89@gmail.com>
2017-05-12 22:08:03 +0200

    fix db

--- pyTemp.js
+++ pyTemp.js
@@ -19,13 +19,13 @@ sensor.read(11, 4, function(err, temperature, humidity) {
       humidity: humidity.toFixed(1),
       temp: temperature.toFixed(1)
     });
-    db.once('open', function() {
-      console.log("open");
-      tempNow.save(function(err) {
-        console.log("Lagret");
-        if (err) return handleError(err);
-        mongoose.disconnect();
-      });
-    });
   }
+});
+db.once('open', function() {
+  console.log("open");
+  tempNow.save(function(err) {
+    console.log("Lagret");
+    if (err) return handleError(err);
+    mongoose.disconnect();
+  });
 });
