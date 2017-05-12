0db27b193d1ccb99372fd0293d7a44f2854d27ac
Kim Tore Kristensen <kimelim89@gmail.com>
2017-05-12 22:21:10 +0200

    fix db

--- pyTemp.js
+++ pyTemp.js
@@ -3,29 +3,29 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var cron = require('node-cron');
 var TempDB = require('./temp.js');
-var tempNow;
+var tempr;
+var humir;
 
 var dato = new Date();
 dato.setHours(dato.getHours() + 2);
 mongoose.Promise = global.Promise;
 db = mongoose.createConnection('mongodb://localhost/templogh');
 db.on('error', console.error.bind(console, 'connection error:'));
-var tempNow = new TempDB({
-  date: dato,
-  humidity: 17,
-  temp: 18
-});
 
 sensor.read(11, 4, function(err, temperature, humidity) {
   console.log(humidity.toFixed(1) + " temperatur");
   if (!err) {
-    var tempNow = new TempDB({
-      date: dato,
-      humidity: humidity.toFixed(1),
-      temp: temperature.toFixed(1)
-    });
+    tempr = temperature.toFixed(1);
+    humir = humidity.toFixed(1);
   }
 });
+
+var tempNow = new TempDB({
+  date: dato,
+  humidity: humir,
+  temp: tempr
+});
+
 db.once('open', function() {
   console.log("open");
   tempNow.save(function(err) {
